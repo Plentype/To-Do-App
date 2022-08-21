8d63e20bc8d397fca402f7136f67f9fe89d75872
Plentype <110918353+Plentype@users.noreply.github.com>
2022-08-21 15:39:01 +0200

    Added an animation

--- script.js
+++ script.js
@@ -1,113 +1,124 @@
 const taskInput = document.getElementById('task-input');
 const dateInput = document.getElementById('date-input');
-taskInput.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter'){
-        event.preventDefault();
-        addListElement();
-    }
-})
-
-dateInput.addEventListener('keypress', (event) => {
-    if(event.key === 'Enter'){
-        event.preventDefault();
-        addListElement();
-    }
-})
+window.addEventListener('keypress', (event) => {
+	if (event.key === 'Enter') {
+		event.preventDefault();
+		addListElement();
+	}
+});
 
 function addListElement() {
-    let checkMarkClicks = 0;
-    const task = document.getElementById('task-input').value;
-    if(task === ''){
-        alert('You must enter a value!');
-        return;
-    }
+	let checkMarkClicks = 0;
+	const task = document.getElementById('task-input').value;
+	if (task === '') {
+		alert('You must enter a value!');
+		return;
+	}
 
-    const todos = document.getElementById('items');
-    let deadline = document.getElementById('date-input').value;
-    
-    const li = document.createElement('li');
-    li.classList.add('todo-list');
+	const todos = document.getElementById('items');
+	let deadline = document.getElementById('date-input').value;
 
-    // Create the checkmark button
-    const checkmark = document.createElement('i');
-    checkmark.classList.add('fa-solid');
-    checkmark.classList.add('fa-2xl');
+	const li = document.createElement('li');
+	li.classList.add('todo-list');
 
-    const checkButton = document.createElement('button');
-    checkButton.appendChild(checkmark);
-    checkButton.classList.add('check-button');
-    checkButton.addEventListener('click', () => {
-        if(checkMarkClicks % 2 === 0) {
-            checkButton.querySelector('i').classList.add('fa-check');
-            checkButton.parentElement.querySelector('h1').style.textDecorationLine = 'line-through';
-            checkButton.parentElement.querySelector('h1').style.textDecorationColor = 'black';
-            checkButton.parentElement.querySelector('h1').style.textDecorationStyle = 'solid';
-            checkButton.parentElement.querySelector('h1').style.textDecorationThickness = '4px';
-            checkMarkClicks++;
-        } else {
-            checkButton.parentElement.querySelector('h1').style.textDecoration = 'none';
-            checkButton.querySelector('i').classList.remove('fa-check');
-            checkMarkClicks++;
-        }
-    })
+	// Create the checkmark button
+	const checkmark = document.createElement('i');
+	checkmark.classList.add('fa-solid');
+	checkmark.classList.add('fa-2xl');
 
-    // Create the task
+	const checkButton = document.createElement('button');
+	checkButton.appendChild(checkmark);
+	checkButton.classList.add('check-button');
+	checkButton.addEventListener('click', () => {
+		if (checkMarkClicks % 2 === 0) {
+			checkButton.querySelector('i').classList.add('fa-check');
+			checkButton.parentElement.querySelector(
+				'h1'
+			).style.textDecorationLine = 'line-through';
+			checkButton.parentElement.querySelector(
+				'h1'
+			).style.textDecorationColor = 'black';
+			checkButton.parentElement.querySelector(
+				'h1'
+			).style.textDecorationStyle = 'solid';
+			checkButton.parentElement.querySelector(
+				'h1'
+			).style.textDecorationThickness = '4px';
+			checkMarkClicks++;
+		} else {
+			checkButton.parentElement.querySelector('h1').style.textDecoration =
+				'none';
+			checkButton.querySelector('i').classList.remove('fa-check');
+			checkMarkClicks++;
+		}
+	});
 
-        const taskDisplay = document.createElement('div');
+	// Create the task
 
-        // Create the task text
-        const todoInput = document.createElement('h1');
-        todoInput.appendChild(document.createTextNode(task));
-        todoInput.classList.add('task-desc');
-        todoInput.setAttribute('title', task);
+	const taskDisplay = document.createElement('div');
 
-        // Create the deadline
-        const dateAndTime = document.createElement('span');
-        if(deadline !== '') {
-            const calendarIcon = document.createElement('i');
-            calendarIcon.classList.add('fa-solid');
-            calendarIcon.classList.add('fa-calendar');
-            calendarIcon.classList.add('fa-lg');
+	// Create the task text
+	const todoInput = document.createElement('h1');
+	todoInput.appendChild(document.createTextNode(task));
+	todoInput.classList.add('task-desc');
+	todoInput.setAttribute('title', task);
 
-            const time = document.createElement('h1');
-            const dateArray = deadline.split('T');
-            const timeArray = dateArray[0].split('-');
-            deadline = dateArray[1] + ' ' + timeArray[2] + '.' + timeArray[1] + '.' + timeArray[0];
-            time.appendChild(document.createTextNode(deadline));
-            dateAndTime.appendChild(calendarIcon);
-            dateAndTime.appendChild(time);
-            dateAndTime.classList.add('deadline-display');
-        }
+	// Create the deadline
+	const dateAndTime = document.createElement('span');
+	if (deadline !== '') {
+		const calendarIcon = document.createElement('i');
+		calendarIcon.classList.add('fa-solid');
+		calendarIcon.classList.add('fa-calendar');
+		calendarIcon.classList.add('fa-lg');
 
-    // Create the remove button
-    const removeButtonIcon = document.createElement('i');
-    removeButtonIcon.classList.add('fa-solid');
-    removeButtonIcon.classList.add('fa-trash-can');
-    removeButtonIcon.classList.add('fa-xl');
+		const time = document.createElement('h1');
+		const dateArray = deadline.split('T');
+		const timeArray = dateArray[0].split('-');
+		deadline =
+			dateArray[1] +
+			' ' +
+			timeArray[2] +
+			'.' +
+			timeArray[1] +
+			'.' +
+			timeArray[0];
+		time.appendChild(document.createTextNode(deadline));
+		dateAndTime.appendChild(calendarIcon);
+		dateAndTime.appendChild(time);
+		dateAndTime.classList.add('deadline-display');
+	}
 
-    const removeButton = document.createElement('button');
-    removeButton.classList.add('remove-button');
-    removeButton.appendChild(removeButtonIcon);
+	// Create the remove button
+	const removeButtonIcon = document.createElement('i');
+	removeButtonIcon.classList.add('fa-solid');
+	removeButtonIcon.classList.add('fa-trash-can');
+	removeButtonIcon.classList.add('fa-xl');
 
-    removeButton.addEventListener('click', () => {
-        removeButton.parentElement.remove();
-    })
+	const removeButton = document.createElement('button');
+	removeButton.classList.add('remove-button');
+	removeButton.appendChild(removeButtonIcon);
 
-    // Wrap checkButton and todoInput in a div
-    const spanWrapper = document.createElement('span');
-    spanWrapper.appendChild(checkButton);
-    spanWrapper.appendChild(todoInput);
-    spanWrapper.classList.add('task-container');
+	removeButton.addEventListener('click', () => {
+		removeButton.parentElement.remove();
+	});
 
-    // Add all elements to the li
-    li.appendChild(spanWrapper);
-    if(dateAndTime.hasChildNodes())
-        li.appendChild(dateAndTime);
-    li.appendChild(removeButton);
+	// Wrap checkButton and todoInput in a div
+	const spanWrapper = document.createElement('span');
+	spanWrapper.appendChild(checkButton);
+	spanWrapper.appendChild(todoInput);
+	spanWrapper.classList.add('task-container');
 
-    // Add li to ul
-    document.getElementById('items').appendChild(li);
+	// Add all elements to the li
+	li.appendChild(spanWrapper);
+	if (dateAndTime.hasChildNodes()) li.appendChild(dateAndTime);
+	li.appendChild(removeButton);
 
-    // Clear input
-    document.getElementById('task-input').value = '';
+	// Add li to ul
+	document.getElementById('items').appendChild(li);
+
+	// Clear input
+	document.getElementById('task-input').value = '';
+
+	// Scroll to the bottom of the page
+	window.scrollTo(0, document.body.scrollHeight);
 }
